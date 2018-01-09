5f3f399a3b7138e19199b4cbb1eae6e73f07ec8b
Rick Haffey <rhaffey@fanatics.com>
2018-01-09 06:42:31 -0600

    chapter 2 notes - part 2

--- chapter02.py
+++ chapter02.py
@@ -93,3 +93,68 @@ c = 1.2 + 3.45j
 # backslash "\" is used to escape special characters
 
 # in python 3, all strings are Unicode
+
+# unicode code point escape
+# - \u for code points in range U+0000 to U+FFFF
+#   "\u269B"
+# - \U for code points in range U+10000 and above
+#   "\U00010133"
+# - \N for unicode descriptive names
+#   "\N{GREEK CAPITAL LETTER OMEGA}"
+
+# raw strings - precede with 'r' or 'R'
+# - note: cannot end in backslash
+# r"users\rhaffey\desktop"
+
+# ## containers: [], (), {}
+# can span multiple lines without "\"
+# trailing comma is allowed
+myList = [
+    1,
+    2,
+    3,
+]
+
+# ## operators, delimiters, and special symbols
+# "$" and "?" have no meaning and are only allowed within quoted strings
+
+
+# ## documentation strings - if first line of module, class, or function is
+# a string, it will become the documentation string for that object
+# - indentation must be consistent with other stmt defs
+# - must always be a string literal in quotes
+def foo(fx):
+    "this function doesn't really do anything"
+    print("hi from foo")
+    return fx
+
+
+def bar(fx):
+    "this doesn't either"
+    print("hi from bar")
+    return fx
+
+
+print(foo.__doc__)
+
+# ## decorators
+# function, method, or class definitions may be preceded by a special symbol
+# known as a decorator - modifies the behavior of the defined
+# function/method/class
+# - denoted with '@'
+# - must be on separate line before the function, method, or class
+
+
+@foo
+@bar
+def baz():
+    print("hi from baz")
+
+
+baz()
+
+# ## source code encoding
+# - default: UTF-8 (py 3)
+# - can add a special encoding comment at start of file to override that:
+
+# -*- coding: <encoding name> -*-
