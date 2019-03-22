f94b17e103127311f62eb0b900ea2f90836500be
Rick Haffey <rhaffey@fanatics.com>
2019-03-22 07:02:06 -0500

    chapter 12 - part 2

---
+++ chapter12.notes.md
@@ -0,0 +1,35 @@
+
+## Builtin Function Categories
+
+### Functional / Collections
+
+* all
+* any
+
+### Math
+
+* abs
+
+
+### String Representations
+
+* ascii
+* bin
+
+### Debugging
+
+* breakpoint
+
+### Type / Object Handling and Interrogation
+
+* callable
+
+
+## Builtin Types
+
+* bool
+* bytearray
+* bytes
+ 
+ 
+ 
--- chapter12.py
+++ chapter12.py
@@ -57,11 +57,19 @@ print("bool({}): {}".format("bye", bool("bye")))
 # >>> bool(): False
 # >>> bool(bye): True
 
-# bytearray([x]) - Type representing a mutable array of bytes.
-# x can be one of the following
+# breakpoint() - Drop into the debugger
+
+breakpoint()
+# >>> --Call--
+# >>> > ~/.../lib/python3.7/site-packages/IPython/core/displayhook.py(247)__call__()
+# >>> -> def __call__(self, result=None):
+
+# bytearray([source[, encoding[, errors]]]) - Type representing a mutable array of bytes.
+# source can be one of the following
 # - sequence of integers in the range 0 to 255
-# - an 8-bit string or bytes literal
+# - a string, with an optional encoding (default is utf-8) and encoding error handling scheme
 # - an integer representing the size of the array
+# - an object conforming to the `buffer` interface
 y = [0, 1, 2, 3, 4, 5]
 print("bytearray({}): {}".format(y, bytearray(y)))
 # >>> bytearray([0, 1, 2, 3, 4, 5]): bytearray(b'\x00\x01\x02\x03\x04\x05')
@@ -88,3 +96,110 @@ print("y: {}".format(y))
 
 # - provides all operations associated with strings
 #   (preface all strings with `b`)
+
+# bytes([x]) -- Type representing an immutable array of bytes.
+# - an immutable version of `bytearray`
+# - sim arguments to that method
+y = [0, 1, 2, 3, 4, 5]
+print("bytes({}): {}".format(y, bytes(y)))
+# >>> bytes([0, 1, 2, 3, 4, 5]): b'\x00\x01\x02\x03\x04\x05'
+
+# main diff: immutable
+# - attempts to update fail:
+y = bytes([10, 20, 30, 40])
+print("y: {}".format(y))
+y[3] = 42
+print("y: {}".format(y))
+
+
+# >>> y: b'\n\x14\x1e('
+# >>> ---------------------------------------------------------------------------
+# >>> TypeError                                 Traceback (most recent call last)
+# >>> <ipython-input-12-ab9ad72fa478> in <module>
+# >>>       1 y = bytes([10, 20, 30, 40])
+# >>>       2 print("y: {}".format(y))
+# >>> ----> 3 y[3] = 42
+# >>>       4 print("y: {}".format(y))
+# >>>
+# >>> TypeError: 'bytes' object does not support item assignment
+
+# callable(object) - returns True if the object appears callable, False if not
+
+def demo_function():
+    print('demo')
+
+
+print("callable(demo_function): {}".format(callable(demo_function)))
+print("callable(42): {}".format(callable(42)))
+# >>> callable(demo_function): True
+# >>> callable(42): False
+
+# ====================
+
+# chr(x)
+# classmethod(func)
+# cmp(x, y)
+# compile(string, filename, kind [, flags [, dont_inherit]])
+# complex([real [, imag]])
+# delattr(object, attr)
+# dict([m])
+# dict(key1 = value1, key2 = value2, ...)
+# dir([object])
+# divmod(a, b)
+# enumerate(iter [, initial_value])
+# eval(expr [, globals [, locals]])
+# exec(code [, global [, locals]])
+# filter(function, iterable)
+# float([x])
+# format(value [, format_spec])
+# frozenset([items])
+# getattr(object, name [, default])
+# globals()
+# hasattr(object, name)
+# hash(object)
+# help(object)
+# hex(x)
+# id()
+# input([prompt])
+# int(x [, base])
+# isinstance(object, classobj)
+# issublclass(class1, class2)
+# iter(object [, sentinel])
+# len(s)
+# list([items])
+# locals()
+# long([x [, base]])
+# map(function, items, ...)
+# max(x [, args, ...])
+# memoryview()
+# min(s [, args, ...])
+# next(s [, default])
+# object()
+# oct(x)
+# open(filename [, mode [, bufsize [, encoding [, errors [, newline [, closefd]]]]]])
+# open(filename [, mode [, bufsize]])
+# ord(d)
+# pow(x, y [, z])
+# print(value, ... [, sep=separator, end=ending, file=outfile])
+# property([fget [, fset [, fdel [, doc]]]])
+# range([start, ] stop [, step])
+# raw_input([prompt])
+# repr(object)
+# reversed(s)
+# round(x [, n])
+# set([items])
+# setattr(object, name, value)
+# slice([start, ] stop [, step])
+# sorted(iterable [, key=keyfunc [, reverse=reverseflag]])
+# staticmethod(func)
+# str([object])
+# sum(items [, initial])
+# super(type [, object])
+# tuple([items])
+# type(name, bases, dict)
+# unichr(x)
+# unicode(string [, encoding [, errors]])
+# vars([object])
+# xrange([start, ] stop [, step])
+# zip([s1 [, s3 [, ...]]])
+# __import__()
